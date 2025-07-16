f4e115761ca8dcb8c4978d1de6538f204686d42a
nojong99 <nojong99@gmail.com>
2025-07-16 17:10:26 +0900

    hell0

--- project/src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ project/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -16,6 +16,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
+                .orElseThrow(() -> new UsernameNotFoundException("사용자는 찾을 수 없습니다: " + username));
     }
 } 
