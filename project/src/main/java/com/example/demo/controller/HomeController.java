2d880200c43c67a4163fbf7faaadb6610cf7eaf6
nojong99 <nojong99@gmail.com>
2025-07-16 17:30:26 +0900

    hello world!

--- project/src/main/java/com/example/demo/controller/HomeController.java
+++ project/src/main/java/com/example/demo/controller/HomeController.java
@@ -8,9 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "씨아이씨디 테스트<br>" +
-               "<a href='http://localhost:8000'></a><br>" +
-               "<a href='/h2-console'</a><br>" +
-               "<a href='/api/v1/test/public'></a>";
+        return "<h1>씨아이씨디 테스트</h1>";
     }
 } 
