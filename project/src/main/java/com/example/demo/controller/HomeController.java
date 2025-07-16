58e361885642b103c304d71db143b348a69eee46
nojong99 <nojong99@gmail.com>
2025-07-16 16:07:16 +0900

    ci!cd!

--- project/src/main/java/com/example/demo/controller/HomeController.java
+++ project/src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "CICD!<br>" +
+        return "CI!CD!<br>" +
                "<a href='http://localhost:8000'>React 프론트엔드로 이동</a><br>" +
                "<a href='/h2-console'>H2 데이터베이스 콘솔</a><br>" +
                "<a href='/api/v1/test/public'>공개 API 테스트</a>";
