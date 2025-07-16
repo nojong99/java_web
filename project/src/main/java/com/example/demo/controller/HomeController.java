65a38e1ff6a1ec26126ec0864cc8895123e22c66
nojong99 <nojong99@gmail.com>
2025-07-16 16:34:25 +0900

    test2

--- project/src/main/java/com/example/demo/controller/HomeController.java
+++ project/src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "씨아이씨디 테스트입니다.<br>" +
+        return "씨아이씨디 테스트<br>" +
                "<a href='http://localhost:8000'>React 프론트엔드로 이동</a><br>" +
                "<a href='/h2-console'>H2 데이터베이스 콘솔</a><br>" +
                "<a href='/api/v1/test/public'>공개 API 테스트</a>";
